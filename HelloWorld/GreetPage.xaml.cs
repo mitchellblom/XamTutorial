fc4a2ef1bd370835f80075579dfee18456e0eee5
Loaner #1 <loaner1@Loaners-MacBook-Air.local>
2017-09-09 13:52:07 -0500

    tutorial progress

---
+++ HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                Padding = new Thickness(0, 20, 0, 0);
+            }
+        }
+    }
+
+}

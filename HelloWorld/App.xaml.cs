97e80ec7dbd58999544095cafaefef0b18a4f408
Loaner #1 <loaner1@Loaners-MacBook-Air.local>
2017-09-09 16:31:40 -0500

    stackpage layout ex 1

--- HelloWorld/App.xaml.cs
+++ HelloWorld/App.xaml.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
                 DependencyService.Register<CloudDataStore>();
 
             if (Device.RuntimePlatform == Device.iOS)
-                MainPage = new GreetPage();
+                MainPage = new StackPagge();
             else
                 MainPage = new NavigationPage(new MainPage());
         }

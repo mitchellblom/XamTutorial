690ff5800a54ac755631f9e89594d213c3960c93
Loaner #1 <loaner1@Loaners-MacBook-Air.local>
2017-09-09 16:21:39 -0500

    working quote miniapp

--- HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/GreetPage.xaml.cs
@@ -2,23 +2,59 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 
 namespace HelloWorld
 {
     public partial class GreetPage : ContentPage
     {
+        private int _i = 0;
+        private string[] _quotes = new string[]
+        {
+            "Life is like a box of chocolates.",
+            "I'm sorry, Wilson!",
+            "Houston, we have a problem."
+        };
 
         public GreetPage()
         {
             InitializeComponent();
 
-            slider.Value = 0.5;
+            currentQuote.Text = _quotes[_i];
 
-            if (Device.RuntimePlatform == Device.iOS)
-            {
-                Padding = new Thickness(0, 20, 0, 0);
-            }
+            slider.Value = 14;
+
+            //Padding = GetPagePadding();
+
         }
-    }
 
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+            _i++;
+            if (_i >= _quotes.Length)
+                _i = 0;
+
+            currentQuote.Text = _quotes[_i];
+		}
+
+        private static Thickness GetPagePadding()
+        {
+            double topPadding;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    topPadding = 20;
+                    break;
+                default:
+                    topPadding = 0;
+                    break;
+            }
+
+            return new Thickness(5, topPadding, 5, 0);
+        }
+
+    }
 }

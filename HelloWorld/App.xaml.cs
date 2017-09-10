5fb32b7f94050907d6fb0a4f8202a3f22b7bac27
mitchellblom <mitchellblom@gmail.com>
2017-09-10 12:42:23 -0500

    relative layout xaml

--- HelloWorld/App.xaml.cs
+++ HelloWorld/App.xaml.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
                 DependencyService.Register<CloudDataStore>();
 
             if (Device.RuntimePlatform == Device.iOS)
-                MainPage = new GridPage();
+                MainPage = new RelativePage();
             else
                 MainPage = new NavigationPage(new MainPage());
         }

d57b2e03030aed12c90ca0448116b5d3ef7461ac
mitchellblom <mitchellblom@gmail.com>
2017-09-10 12:59:48 -0500

    relative layout code behind

--- HelloWorld/RelativePage.xaml.cs
+++ HelloWorld/RelativePage.xaml.cs
@@ -10,6 +10,18 @@ namespace HelloWorld
         public RelativePage()
         {
             InitializeComponent();
+
+            var layout = new RelativeLayout();
+            Content = layout;
+
+            var aquaBox = new BoxView { Color = Color.Aqua };
+            layout.Children.Add(aquaBox,
+               widthConstraint: Constraint.RelativeToParent(parent => parent.Width),
+               heightConstraint: Constraint.RelativeToParent(parent => parent.Height * 0.3));
+
+            var silverBox = new BoxView { Color = Color.Silver };
+            layout.Children.Add(silverBox,
+                yConstraint: Constraint.RelativeToView(aquaBox, (RelativeLayout, element) => element.Height + 20));
         }
     }
 }

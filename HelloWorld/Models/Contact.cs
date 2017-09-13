596b3f482bb6e4447b817bb297490d4c0f1ed721
mitchellblom <mitchellblom@gmail.com>
2017-09-13 11:36:39 -0500

    list with images and details

---
+++ HelloWorld/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HelloWorld.Models
+{
+    public class Contact : ContentPage
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

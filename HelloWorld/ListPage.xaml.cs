0e263224672ddb7388bd489af7b36b828a3f78ae
mitchellblom <mitchellblom@gmail.com>
2017-09-13 11:47:51 -0500

    list stack with image and deets

--- HelloWorld/ListPage.xaml.cs
+++ HelloWorld/ListPage.xaml.cs
@@ -15,7 +15,7 @@ namespace HelloWorld
             listView.ItemsSource = new List<Contact>{
                 new Contact { Name = "Mosh", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/139.jpg"},
                 new Contact { Name = "John", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/140.jpg", Status="Yo dude"},
-                new Contact { Name = "Bob", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/138.jpg", Status="Hey, let's talk!"}
+                new Contact { Name = "Bob", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/139.jpg", Status="Hey, let's talk!"}
                
             };
         }

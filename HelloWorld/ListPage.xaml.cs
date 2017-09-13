dc0b3042b7866a08e88a35cfd41fd227a27b9c73
mitchellblom <mitchellblom@gmail.com>
2017-09-13 11:59:43 -0500

    list grouping by titles

--- HelloWorld/ListPage.xaml.cs
+++ HelloWorld/ListPage.xaml.cs
@@ -12,12 +12,19 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            listView.ItemsSource = new List<Contact>{
-                new Contact { Name = "Mosh", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/139.jpg"},
-                new Contact { Name = "John", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/140.jpg", Status="Yo dude"},
-                new Contact { Name = "Bob", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/139.jpg", Status="Hey, let's talk!"}
-               
+            listView.ItemsSource = new List<ContactGroup>{
+
+				new ContactGroup("M peeps", "M"){
+					new Contact { Name = "Mary", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/140.jpg", Status="Yo dude"},
+					new Contact { Name = "Mosh", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/139.jpg"}
+				},
+
+				new ContactGroup("J people", "J"){
+					new Contact { Name = "John", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/140.jpg", Status="Yo dude"},
+					new Contact { Name = "Jimbo", ImageUrl = "http://placekitten.com.s3.amazonaws.com/homepage-samples/96/139.jpg", Status="Hey, let's talk!"}
+				}
             };
+
         }
     }
 }
---
+++ HelloWorld/Models/ContactGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HelloWorld.Models
+{
+    public class ContactGroup : List<Contact> 
+    {
+
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+
+        public ContactGroup(string title, string shortTitle)
+        {
+            Title = title;
+            ShortTitle = shortTitle;
+        }
+    }
+}
+

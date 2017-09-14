53d7cdb66c581213b7128797813ab58914613ff7
mitchellblom <mitchellblom@gmail.com>
2017-09-14 09:32:23 -0500

    toolbars like add item

---
+++ HelloWorld/ToolbarPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class ToolbarPage : ContentPage
+    {
+        void Handle_Activated(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Activated", "ToolbarItem Activated", "OK");
+        }
+
+        public ToolbarPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

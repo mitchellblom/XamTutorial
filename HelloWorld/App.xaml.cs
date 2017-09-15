c42ea747930174373730c434a11bfa2a4d2bfe08
mitchellblom <mitchellblom@gmail.com>
2017-09-15 10:13:21 -0500

    date time pickers

--- HelloWorld/App.xaml.cs
+++ HelloWorld/App.xaml.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
                 DependencyService.Register<CloudDataStore>();
 
             //MainPage = new NavigationPage (new ContactsPage()){  
-            MainPage = new PickerPage() {
+            MainPage = new DateTimePage() {
                 //BarBackgroundColor = Color.Gray,
                 //BarTextColor = Color.White
             };
---
+++ HelloWorld/DateTimePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class DateTimePage : ContentPage
+    {
+        void Handle_DateSelected(object sender, Xamarin.Forms.DateChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTimePage()
+        {
+            InitializeComponent();
+        }
+    }
+}

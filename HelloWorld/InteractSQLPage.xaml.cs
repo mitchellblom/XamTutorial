c79283471a73a5ff56e98207f6b4c4a4162f4fef
mitchellblom <mitchellblom@gmail.com>
2017-09-15 15:03:47 -0500

    persisting updates

--- HelloWorld/InteractSQLPage.xaml.cs
+++ HelloWorld/InteractSQLPage.xaml.cs
@@ -2,16 +2,39 @@
 using HelloWorld.Persistence;
 using SQLite;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace HelloWorld
 {
-    public class Recipe
+    public class Recipe : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
+        private string _name;
+
         [MaxLength(255)]
-        public string Name { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value)
+                    return;
+
+                _name = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
     public partial class InteractSQLPage : ContentPage
